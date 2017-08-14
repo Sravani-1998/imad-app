1ce50fab8cd84ec7d9d95506a3af5f1720fb6e14
Sravani Kothakota <30896617+Sravani-1998@users.noreply.github.com>
2017-08-14 11:33:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.send("Atricle one is being served here") ;
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
